3be1a3c5a2a90ac7eda69582c8f18f08e2ad1342
Ajay H <you@example.com>
2025-06-21 09:51:14 +0530

    add: Create custom User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from  django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#Custom user model
+class User(AbstractUser):
+    email = models.EmailField(unique=True,blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return f'{self.username} - {self.email}'
+
+
+
+
+
+
+
+
+
+
+
