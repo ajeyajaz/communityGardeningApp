2ef7fce938f089ffec813a79592d1874bd82b94b
Ajay H <you@example.com>
2025-06-21 13:03:04 +0530

    chore: Initial migration for profile model

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import UserSignUpAPIView
+
+
+
+urlpatterns = [
+    path('signup/',UserSignUpAPIView.as_view(), name='signup-api'),
+
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+from .serializers import UserSignUpSerializer
+
+
+class UserSignUpAPIView(CreateAPIView):
+    serializer_class = UserSignUpSerializer
+
+
