9bf946466a2a07c0c13c841486df06f1d9d375e3
Ajay H <you@example.com>
2025-06-21 17:20:03 +0530

    add: add api endpoints for auth and profile

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import UserSignUpAPIView
+from .views import (
+    UserSignUpAPIView,
+    UserProfileCreateAPIView,
+    UserProfileUpdateAPIView,
+    UserList,
+)
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -7,6 +12,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('signup/',UserSignUpAPIView.as_view(), name='signup-api'),
-    path('login/',TokenObtainPairView.as_view(),name='login')
+    path('login/',TokenObtainPairView.as_view(), name='login'),
+
+    path('profile/',UserProfileCreateAPIView.as_view(), name='profile-create-api'),
+    path('profile/<int:pk>/',UserProfileUpdateAPIView.as_view(), name='profile-update-api'),
+
+    path('list/',UserList.as_view())
 
 ]
